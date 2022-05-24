28f7e0333a8837501d9f592c51cf25ee50f2945b
Jonathan Down <jdown@cardataconsultants.com>
2022-05-24 15:11:14 -0400

    bugfix for php 7.3

--- src/PendingCommand.php
+++ src/PendingCommand.php
@@ -409,7 +409,9 @@ class PendingCommand
 
         foreach ($this->test->expectedOutputSubstrings as $i => $text) {
             $mock->shouldReceive('doWrite')
-                ->withArgs(fn ($output) => str_contains($output, $text))
+                ->withArgs(function ($output) use ($text) {
+					return str_contains($output, $text);
+				})
                 ->andReturnUsing(function () use ($i) {
                     unset($this->test->expectedOutputSubstrings[$i]);
                 });
@@ -426,7 +428,9 @@ class PendingCommand
 
         foreach ($this->test->unexpectedOutputSubstrings as $text => $displayed) {
             $mock->shouldReceive('doWrite')
-                 ->withArgs(fn ($output) => str_contains($output, $text))
+                 ->withArgs(function ($output) use ($text) {
+					 return str_contains($output, $text);
+				 })
                  ->andReturnUsing(function () use ($text) {
                      $this->test->unexpectedOutputSubstrings[$text] = true;
                  });
